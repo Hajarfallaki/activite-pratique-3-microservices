a3a4326bdc9d0ee23b28fe36006412c49d653ec0
Hajarfallaki <hajarfallaki24@gmail.com>
2025-12-28 21:29:07 +0100

    premier commit

---
+++ src/main/java/ma/enset/java/ecomapp/EcomAppApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.java.ecomapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomAppApplication.class, args);
+	}
+
+}
